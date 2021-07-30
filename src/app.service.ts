eecec63ea82c874e1bdf923f2e35af8d201c2a54
Pyakz <MarkVergelBanguis@gmail.com>
2021-07-30 15:06:05 +0800

    :hammer: README.md

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `
+    <div class='' >
+    <a href='/graphql'> Click here to use the playground </a>
+    </div>
+    `;
   }
 }
